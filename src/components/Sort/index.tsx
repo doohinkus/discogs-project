7e591b877fff7e34d2ce9c5d91d4c7edd86d1c13
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2021-09-08 16:50:30 -0700

    add filter to select

--- src/components/Sort/index.tsx
+++ src/components/Sort/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 
 export default function Sort({ ...props }) {
+  function handleSelect(e: any) {
+    // console.log(e.target.value);
+    if (e.target.value === "title") {
+      props.handleSortByTitle();
+    } else {
+      props.handleSortByYear();
+    }
+  }
   return (
     <nav id="sortOptions" aria-label="sorters">
       <div id="sort">Sort by:</div>
-      <select aria-labelledby="sort">
+      <select
+        aria-labelledby="sort"
+        name="filterSelection"
+        defaultValue=""
+        onChange={handleSelect}
+      >
         <option>title</option>
         <option>year</option>
       </select>
