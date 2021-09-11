14fcd1d2da947c5cfed180355b32dad2d8676895
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2021-09-11 12:27:25 -0700

    complete mobile view with new styles

--- src/components/Album/index.tsx
+++ src/components/Album/index.tsx
@@ -19,21 +19,23 @@ export default function Album({
     <article>
       <section className={styles.album}>
         <div className={styles.image}>
-          <a
-            href={`https://www.discogs.com/release/${id}`}
-            aria-label={`Disover more about ${title} with ${id}`}
-          >
-            <img
-              src={thumb?.length > 0 ? thumb : "https://fakeimg.pl/150x150/"}
-              className={styles.thumb}
-              alt={`Album cover for ${title}`}
-            />
-          </a>
+          <img
+            src={thumb?.length > 0 ? thumb : "https://fakeimg.pl/150x150/"}
+            className={styles.thumb}
+            alt={`Album cover for ${title}`}
+          />
           <div className={styles.overlay}>
             <h2 className={styles.title}>{title}</h2>
             <p className={styles.artist}>{artist}</p>
             <p className={styles.year}>{year}</p>
             <p className={styles.catno}>{catno}</p>
+            <a
+              href={`https://www.discogs.com/release/${id}`}
+              className={styles.link}
+              aria-label={`Disover more about ${title} with ${id}`}
+            >
+              View Details
+            </a>
           </div>
         </div>
       </section>
