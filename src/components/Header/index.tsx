2dc270639e29be2729812b44fd7113c7b6b052b7
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2021-09-11 14:28:59 -0700

    update styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import styles from "./Header.module.css";
 export default function Header({ ...props }) {
   return (
     <header>
-      <h1>Discogs Release Explorer</h1>
+      <h1 className={styles.header}>Discogs Release Explorer</h1>
     </header>
   );
 }
